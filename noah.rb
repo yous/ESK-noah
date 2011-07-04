2505da71416d1aa6c50bc53e73ac5feb6199c1a7
yous <chacha@kaist.ac.kr>
2011-07-04 00:57:49 -0700

    inner_html의 첫 줄만 출력

--- noah.rb
+++ noah.rb
@@ -4,4 +4,4 @@ require 'mechanize'
 a = Mechanize.new
 r = a.get 'http://noah.kaist.ac.kr/divisionCS/Notice'
 puts r.body
-puts r.search('//table/tr/td[@width="55%"]/a[@class="articlelink"]').map {|v| v.inner_html}
+puts r.search('//table/tr/td[@width="55%"]/a[@class="articlelink"]').map {|v| v.inner_html.split("\n")[0]}
